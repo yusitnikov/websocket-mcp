c612ad1466b9c9f0d0f5bdd3c43e56f517bfd6a1
yusitnikov <yuri.sitnikov@kaltura.com>
2025-09-19 11:22:05 +0300

    move the old build to "archived"

--- src/isMethodNotFoundError.ts
+++
@@ -1,5 +0,0 @@
-import { McpError, ErrorCode } from "@modelcontextprotocol/sdk/types.js";
-
-export const isMethodNotFoundError = (error: unknown) => {
-    return error instanceof McpError && error.code === ErrorCode.MethodNotFound;
-};
